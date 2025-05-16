63083e026428ee44f4dc4e17e20602bc0a330996
vandan <levandanhh2@gmail.com>
2025-05-16 14:04:16 +0700

    interfaceSignUp

---
+++ src/main/java/com/example/vocaboost/controller/SignUpController.java
@@ -0,0 +1,25 @@
+package com.example.vocaboost.controller;
+
+import com.example.vocaboost.model.User;
+import com.example.vocaboost.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping("/homeWeb")
+public class SignUpController {
+    @Autowired
+    private IUserService userService;
+
+    @Value("${file-upload}")
+    private String uploadPath;
+}
---
+++ src/main/java/com/example/vocaboost/service/IUserService.java
@@ -0,0 +1,6 @@
+package com.example.vocaboost.service;
+
+import com.example.vocaboost.model.User;
+
+public interface IUserService extends IGenerateService<User>{
+}
