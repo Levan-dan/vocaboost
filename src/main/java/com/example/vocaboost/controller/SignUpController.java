5dd15e41ee44f50ea62364deb1b30fc54d86905b
vandan <levandanhh2@gmail.com>
2025-05-19 10:31:46 +0700

    logicSignUp

--- src/main/java/com/example/vocaboost/controller/SignUpController.java
+++ src/main/java/com/example/vocaboost/controller/SignUpController.java
@@ -20,34 +20,55 @@ public class SignUpController {
     @Autowired
     private IUserService userService;
 
-    @Value("${file-upload}")
-    private String uploadPath;
 
     @GetMapping("")
-    public String showMainScreenWebVocaboost(){
+    public String showMainScreenWebVocaboost() {
         return "/user/main_screen";
     }
 
     @GetMapping("/showIntroducePage")
-    public String showIntroducePage(){
+    public String showIntroducePage() {
         return "/user/introduce_web";
     }
 
     @GetMapping("/showSignUpForm")
-    public String showScreenSignUp(Model model){
+    public String showScreenSignUp(Model model) {
         model.addAttribute("user", new User());
         return "/user/sign_up";
     }
 
     @GetMapping("/showLogInForm")
-    public String showScreenLogIn(Model model){
+    public String showScreenLogIn(Model model) {
         model.addAttribute("user", new User());
         return "/user/log_in";
     }
 
     @GetMapping("/showPrefacePage")
-    public String showPreface(){
+    public String showPreface() {
         return "/user/preface_page";
     }
+
+    @PostMapping("/signUp")
+    public String signUpToHomeUser(@ModelAttribute("user") User user,
+                                   @RequestParam("avatar") MultipartFile avatar,
+                                   Model model) throws IOException {
+
+        String avatarPath = userService.uploadAvatar(avatar);
+        if (avatarPath != null) {
+            user.setAvatarPath(avatarPath);
+        }
+
+        user.setRole("user");
+        user.setCreate_at(LocalDateTime.now());
+
+        userService.save(user);
+
+//        model.addAttribute("mess", "Đăng ký thành công và ảnh đã được lưu!");
+//        model.addAttribute("name", user.getUsername());
+//        model.addAttribute("imageName", user.getAvatarPath());
+
+        return "/user/surveyQuestion";
+    }
+
 
 }
--- src/main/java/com/example/vocaboost/service/IUserService.java
+++ src/main/java/com/example/vocaboost/service/IUserService.java
@@ -1,6 +1,10 @@
 package com.example.vocaboost.service;
 
 import com.example.vocaboost.model.User;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface IUserService extends IGenerateService<User>{
+    String uploadAvatar(MultipartFile file) throws IOException;
 }
